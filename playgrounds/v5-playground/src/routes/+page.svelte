74b1451bff4d06391b36403c06bacba1376cc52a
Hunter Johnston <johnstonhuntera@gmail.com>
2024-04-11 14:17:12 -0400

    s5

---
+++ playgrounds/v5-playground/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	let { ...rest } = $props();
+</script>
+
+<h1 {...rest}>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
